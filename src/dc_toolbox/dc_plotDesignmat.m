3adc66df914cb23911da4e56480c318461048ecb
behinger <behinger@uos.de>
2017-08-18 15:28:04 +0200

    renamed timeshift to timeexpand

--- src/dc_toolbox/dc_plotDesignmat.m
+++ src/dc_toolbox/dc_plotDesignmat.m
@@ -1,11 +1,11 @@
 function dc_plotDesignmat(EEG,varargin)
 %Plots the designmatrix
-%If the matrix is very large (the timeshifted/dcX matrix) we do not plot
+%If the matrix is very large (the timeexpanded/dcX matrix) we do not plot
 %everything, but only the middle 1000s. We also try to zoom in
 %automatically, but this fails sometimes
 %
 %Arguments:
-%   cfg.timeshift' (boolean):
+%   cfg.timeexpand' (boolean):
 %        0: Plots EEG.deconv.X (default)
 %        1: Plots EEG.deconv.dcX
 %   cfg.logColor(boolean): plot the color on logscale (default 0)
@@ -15,12 +15,12 @@ function dc_plotDesignmat(EEG,varargin)
 %*Example:*
 % dc_plot_designmat(EEG)
 %
-% dc_plot_designmat(EEG,'timeshift',1) %plot the timeshifted X
+% dc_plot_designmat(EEG,'timeexpand',1) %plot the timeexpanded X
 
 % Secret option: 'addContData'
 cfg = finputcheck(varargin,...
     {'event_or_full',   'string', {'event','full','deprecated'}, 'deprecated';...
-    'timeshift',   'boolean',[],0;...
+    'timeexpand',   'boolean',[],0;...
     'logColor','boolean',[0,1],0;...
     'figure','boolean',[],1;...
     'sort','boolean',[0,1],0;...
@@ -29,15 +29,15 @@ cfg = finputcheck(varargin,...
 
 
 if ~strcmp(cfg.event_or_full,'deprecated')
-    warning('Hi Olaf! event_or_full is deprecated and I changed to the boolean parameter ''timeshift'', e.g. timeshift=1 means the old ''full''.')
+    warning('Hi Olaf! event_or_full is deprecated and I changed to the boolean parameter ''timeexpand'', e.g. timeexpand=1 means the old ''full''.')
     switch cfg.event_or_full
         case 'full'
-            cfg.timeshift = 1;
+            cfg.timeexpand = 1;
     end
 end
-if cfg.timeshift
+if cfg.timeexpand
     yAxisLabel = 'time [s]';
-    
+
     time_lim = EEG.times(ceil(end/2)) + [-500,500] * 1000;
     if EEG.xmin > time_lim(1) || EEG.xmax <time_lim(2)
         warning('the design-matrix is too large to display, we show only the middle 1000 seconds.')
@@ -45,13 +45,13 @@ if cfg.timeshift
     time_ix = EEG.times < time_lim(2) & EEG.times > time_lim(1);
     yAxis = EEG.times(time_ix)/1000;
     X = EEG.deconv.dcX(time_ix,:);
-    shiftByOne = 0; % dont shift the XTicks by one 
+    shiftByOne = 0; % dont shift the XTicks by one
 else
     yAxisLabel = 'event number';
     X = EEG.deconv.X;
     yAxis = 1:size(X,1);
     shiftByOne = 1;  % shift the XTicks by one
-    
+
 end
 if cfg.logColor
     X = log(X);
@@ -67,7 +67,7 @@ nPredTheory = length(EEG.deconv.colnames);
 if cfg.figure
     figure
 end
-if cfg.timeshift && cfg.addContData
+if cfg.timeexpand && cfg.addContData
     subplot(1,20,[2:20])
 end
 ig = imagesc(1:nPred,yAxis,X);
@@ -87,7 +87,7 @@ if length(r)>3
 end
 
 
-if cfg.timeshift && cfg.addContData
+if cfg.timeexpand && cfg.addContData
     subplot(1,20,1)
     plot(EEG.data(1,:),EEG.times/1000)
 %     zoom yon
@@ -98,28 +98,28 @@ if cfg.timeshift && cfg.addContData
 
 end
 
-if cfg.timeshift
+if cfg.timeexpand
 %     warning('auto-zoom to useful resolution (25x the stimulus-window)')
 %     warning('XXX could be dimension 2 for splines?')
-    
+
 %     zoom yon
 %     zoom(length(yAxis)./(size(EEG.deconv.dcBasis,1)*25))
     axes(ax)
     pan yon
     allTypes = {EEG.event(:).type};
     lat = [EEG.event(:).latency]/EEG.srate*1000;  %in ms
-    
+
     latix = (lat< (time_lim(2))) & (lat > (time_lim(1)));
     if sum(latix)>0 && sum(latix)<1000 % it starts to get really buggy after 1000 lines
-        
+
         % plot all event onsets as horizontal lines with different colors per event
         [un,~,c] = unique(allTypes(latix));
         colorList = repmat({{'r','g','b','c','m','y','k','w'}},ceil(length(un)/8),1); % ugly and lazy... but who has more than that different types...
         colorList = [colorList{:}];
         hline(lat(latix)/1000,colorList(c))
     end
-    
-    
+
+
     % Zoom to an event that is exactly in the middle of the cut data
     latTmp = lat(latix);
     ylim([latTmp(ceil(end/2))/1000-3 latTmp(ceil(end/2))/1000+3])
