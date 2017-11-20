7a600f66cdf618b558433c1169cfa8047fbd3010
Benedikt Ehinger <behinger@uos.de>
2017-11-20 20:02:02 +0100

    fix

--- src/dc_toolbox/dc_updateFileformat.m
+++ src/dc_toolbox/dc_updateFileformat.m
@@ -1,17 +1,24 @@
 function unfold = dc_updateFileformat(unfold)
 % updates the unfold file struct to some changes in variable names
-unfold.deconv = rename_deconv(unfold.deconv,'dcX','Xdc');
-unfold.deconv = rename_deconv(unfold.deconv,'dcX_termidx','Xdc_terms2cols');
-unfold.deconv = rename_deconv(unfold.deconv,'dcBeta','beta_dc');
-unfold.deconv = rename_deconv(unfold.deconv,'XBeta','beta_nodc');
-unfold.deconv = rename_deconv(unfold.deconv,'dcBasis','timebasis');
-unfold.deconv = rename_deconv(unfold.deconv,'dcX_termidx','Xdc_terms2cols');
-unfold.deconv = rename_deconv(unfold.deconv,'dcBasistime','times');
+unfold.deconv = rename_stru(unfold.deconv,'dcX','Xdc');
+unfold.deconv = rename_stru(unfold.deconv,'dcX_termidx','Xdc_terms2cols');
+unfold.deconv = rename_stru(unfold.deconv,'dcBeta','beta_dc');
+unfold.deconv = rename_stru(unfold.deconv,'XBeta','beta_nodc');
+unfold.deconv = rename_stru(unfold.deconv,'dcBasis','timebasis');
+unfold.deconv = rename_stru(unfold.deconv,'dcX_termidx','Xdc_terms2cols');
+unfold.deconv = rename_stru(unfold.deconv,'dcBasistime','times');
+unfold.deconv = rename_stru(unfold.deconv,'col2eventtype','cols2eventtype');
+
+
+
+unfold = rename_stru(unfold,'epoch','param');
+
 
 end
-function deconv = rename_deconv(deconv,old,new)
-if isfield(deconv,old)
-    deconv.(new) = deconv.(old);
-    deconv = rmfield(deconv,old);
+
+function stru = rename_stru(stru,old,new)
+if isfield(stru,old)
+    stru.(new) = stru.(old);
+    stru = rmfield(stru,old);
 end
 end
