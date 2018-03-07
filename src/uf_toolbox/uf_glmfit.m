f17bb5bf7fbdc5d1a5ae93d67be38301e61f98ac
agert <agert@kalyke.neurobiopsychologie.uni-osnabrueck.de>
2018-03-07 16:56:04 +0100

    added additional warning

--- src/uf_toolbox/uf_glmfit.m
+++ src/uf_toolbox/uf_glmfit.m
@@ -182,9 +182,10 @@ elseif strcmp(cfg.method,'par-lsmr')
         fprintf('\nsolving electrode %d (of %d electrodes in total)',e,length(cfg.channel))
         % use iterative solver for least-squares problems (lsmr)
         [beta(:,e),ISTOP,ITN] = lsmr(parXdc.Value,parData.Value(:,e),[],10^-10,10^-10,[],cfg.lsmriterations); % ISTOP = reason why algorithm has terminated, ITN = iterations
-        if ISTOP == 7
+        if ISTOP == 7 
             warning(['The iterative least squares did not converge for channel ',num2str(e), ' after ' num2str(ITN) ' iterations. You can either try to increase the number of iterations using the option ''lsmriterations'' or it might be, that your model is highly collinear and difficult to estimate. Check the designmatrix EEG.unfold.X for collinearity.'])
-            
+        elseif ITN == cfg.lsmriterations
+            warning(['The iterative least squares (likely) did not converge for channel ',num2str(e), ' after ' num2str(ITN) ' iterations. You can either try to increase the number of iterations using the option ''lsmriterations'' or it might be, that your model is highly collinear and difficult to estimate. Check the designmatrix EEG.unfold.X for collinearity.'])
         end
         fprintf('... took %i iterations and %.1fs',ITN,toc(t))
         
