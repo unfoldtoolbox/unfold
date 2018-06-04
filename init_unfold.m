65cb4d7cc35063e23343f326081e73bc236011fc
Benedikt Ehinger <behinger@uos.de>
2018-06-04 11:02:58 +0200

    added eeglab finputcheck functions

--- init_unfold.m
+++ init_unfold.m
@@ -16,8 +16,15 @@ addpath(genpath(fullfile(projectFolder,'lib','eegvis','topo_butter')))
 addpath(fullfile(projectFolder,'lib','glmnet_matlab'))
 
 if ~exist('eeg_checkset','file')
-    warning('EEGlab could not be found in your path. Please add it before you use this toolbox')
+    try
+        eeglab
+    catch
+    
+    warning('EEGlab could not be found in your path. You should be fine EXCEPT for using uf_epoch (for massive univariate modeling without overlap correction) which requires EEGLAB')
+    addpath(fullfile(projectFolder,'lib','eeglab'))
+    end
 end
+
 if ~exist('gramm','file')
   warning('gramm could not be found. Did you run "git submodule update --init" to initialize submodules?')
 end
