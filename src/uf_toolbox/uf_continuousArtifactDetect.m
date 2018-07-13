48b7693aea532534368262e0395a165a319162e7
olafdimigen <olaf.dimigen@hu-berlin.de>
2018-07-13 11:58:39 +0200

    Some more cosmetic changes to function documentation

--- src/uf_toolbox/uf_continuousArtifactDetect.m
+++ src/uf_toolbox/uf_continuousArtifactDetect.m
@@ -1,9 +1,13 @@
 function [WinRej] = uf_continuousArtifactDetect(EEG,varargin)
 %% Reject commonly recorded artifactual potentials (c.r.a.p.)
-% This function has been altered very much by Benedikt Ehinger
-% I removed all the filter-features
-% I changed the input parser.
 %
+% Note: This is an ERPLAB function that was heavily altered by Benedikt
+% Ehinger to be included in the unfold toolbox. In particular, I removed 
+% all the filter-features and changed the input parser.
+% Please cite the ERPLAB toolbox if you use this function (reference below).
+% Benedikt Ehinger & Olaf Dimigen
+% 
+% -------------------------------------------------------------------------
 % There are a number of common artifacts that you will see in nearly every EEG data file. These
 % include eyeblinks, slow voltage changes (caused mostly by skin potentials), muscle activity
 % (from moving the head or tensing up the muscles in the face or neck), horizontal eye movements,
@@ -18,11 +22,11 @@ function [WinRej] = uf_continuousArtifactDetect(EEG,varargin)
 % artifactual segments will be rejected and replaced by a 'boundary' event code.
 %
 %Arguments:
-% EEG:         - continuous EEG dataset (EEGLAB's EEG structure)
-% 'amplitudeThreshold':     - Thresolds ( values). [-lim +lim] is marked
-% 'windowsize':     - moving window width in msec (default 2000 ms)
-% 'stepsize':    - moving window step (default 1000 ms)
-% 'combineSegments':  - marked segment(s) closer than this value will be joined together.
+% EEG:                  - continuous EEG dataset (EEGLAB's EEG structure)
+% 'amplitudeThreshold': - Thresolds (values). [-lim +lim] is marked
+% 'windowsize':         - moving window width (in msec, default: 2000 ms)
+% 'stepsize':           - moving window step (default: 1000 ms)
+% 'combineSegments':    - marked segment(s) closer than this value will be joined together
 %
 %Example:
 % winrej = uf_continuousArtifactDetect(EEG)
@@ -37,10 +41,8 @@ function [WinRej] = uf_continuousArtifactDetect(EEG,varargin)
 % Davis, CA
 % 2009
 
-'';
-%
 % ERPLAB Toolbox
-% Copyright ï¿½ 2007 The Regents of the University of California
+% Copyright 2007 The Regents of the University of California
 % Created by Javier Lopez-Calderon and Steven Luck
 % Center for Mind and Brain, University of California, Davis,
 % javlopez@ucdavis.edu, sjluck@ucdavis.edu
@@ -60,8 +62,6 @@ function [WinRej] = uf_continuousArtifactDetect(EEG,varargin)
 %
 % Acknowledgment:
 % Thanks to Paul Kieffaber for his advice on improving this function.
-%
-
 
 cfg = finputcheck(varargin,...
     {'amplitudeThreshold',   'integer', [], 150;...
@@ -71,33 +71,28 @@ cfg = finputcheck(varargin,...
     'combineSegments','integer',[],[];...
     },'mode','error');
 
-
-
-
 if(ischar(cfg)); error(cfg);end
 
 % check for EYE-EEG channels
 if ~isempty(EEG.chanlocs) && any(strcmp('EYE',{EEG.chanlocs(cfg.channels).type}))
-    warning('EYE-Channels (from EYE-EEG toolobx) detected. Its not recommended to include these channels in the continuousArtefactDetect algorithm as the scale is usually very differet. Please remove before using this function')
+    warning('EYE-Channels (from EYE-EEG toolbox) detected. It is not recommended to include these channels in the continuousArtefactDetect() function as the scale is usually very different. Please remove before using this function')
 end
 
-ampth     = cfg.amplitudeThreshold;%microV
-winms     = cfg.windowsize;%ms
-stepms    = cfg.stepsize;% ms steps
-chanArray = cfg.channels;
-
-
-shortisi  = cfg.combineSegments;
+ampth     = cfg.amplitudeThreshold; % in µV
+winms     = cfg.windowsize;         % in ms
+stepms    = cfg.stepsize;           % in ms steps
+chanArray = cfg.channels;           % which channels to check?
+shortisi  = cfg.combineSegments;    % merge adjacent bad intervals?
 
 [WinRej, chanrej] = basicrap(EEG, chanArray, ampth, winms, stepms);
 shortisisam  = floor(shortisi*EEG.srate/1000);  % to samples
 
 if isempty(WinRej)
-    fprintf('\n No data found worse than the threshold. No rejection was performed.\n');
+    fprintf('\n No data found exceeding the threshold. No rejection was performed.\n');
 else
     %colorseg = [1.0000    0.9765    0.5294];
     if ~isempty(shortisisam)
-        [WinRej, chanrej ] = joinclosesegments(WinRej, chanrej, shortisisam);
+        [WinRej, chanrej] = joinclosesegments(WinRej, chanrej, shortisisam);
     end
     % Correct overlapping windows, there used to be a bug in EEGLAB and this fixes it.
     throw_out = nan;
@@ -112,12 +107,11 @@ else
                 WinRej(i,1) = min(WinRej(i,1),WinRej(i+1,1));
                 WinRej(i,2) = max(WinRej(i,2),WinRej(i+1,2));
             end
-
+            
         end
         WinRej(throw_out,:) = [];
     end
-
-
+    
     % colormatrej = repmat([1 0 0], size(WinRej,1),1);
     % matrixrej = [WinRej colormatrej chanrej];
     % % call figure
@@ -125,5 +119,5 @@ else
     
     nbad = sum(WinRej(:,2)-WinRej(:,1));
     fprintf('\n %i segments were marked.', size(WinRej,1));
-    fprintf('\n A total of %i samples (%.02f percent of data) was marked as bad.\n\n',nbad,nbad/size(EEG.data,2)*100);    
+    fprintf('\n A total of %i samples (%.02f percent of data) was marked as bad.\n\n',nbad,nbad/size(EEG.data,2)*100);
 end
