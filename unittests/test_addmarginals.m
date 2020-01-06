2e07e4a02d0eb8cd4cfdac4df01091b752d828c1
Benedikt Ehinger <behinger@uos.de>
2020-01-06 17:39:36 +0100

    unittest for bug #82

--- unittests/test_addmarginals.m
+++ unittests/test_addmarginals.m
@@ -9,7 +9,7 @@ cfgDesign = [];
 
 cfgDesign.coding = 'dummy';
 cfgDesign.formula   = {'y~1 + cat(conditionA) + continuousA','y~1'};
-cfgDesign.eventtypes = {{'stimulus2'},{'stimulus3'}};
+cfgDesign.eventtypes = {{'stimulus2'},{'stimulus1','stimulus3'}};
 
 EEG = uf_designmat(EEG,cfgDesign);
 EEG = uf_timeexpandDesignmat(EEG,'timelimits',[-0.5 1.5]);
@@ -27,9 +27,9 @@ ufresult_marginal = uf_addmarginal(ufresult_predict);
 % the stimulus3 sould be unaffected, it's just an intercept
 assert(1==near(ufresult_marginal.beta(:,:,5),ufresult_predict.beta(:,:,5))) % this column should not have been impacted.
 
-% the stimulus2 conditionA should be unaffected, it is a categorical
-% variable
-near(ufresult_marginal.beta(:,:,2),ufresult_predict.beta(:,:,2))
+% the stimulus2 conditionA should be sum of cont av + interaction +
+% categorical predictor
+assert(1==near(ufresult_marginal.beta(:,:,2),sum(ufresult_average.beta(:,:,[1 2 3]),3)))
 
 % the stimulus 2, parameter 3 is continuous = 0, so it should be the same
 % as the intercept
@@ -38,6 +38,31 @@ assert(1==near(ufresult_marginal.beta(:,:,3), ufresult.beta(:,:,1)))
 % the stimulus 2, parameter 1 should be at continuous = mean(continuous)
 % {that's 50} so it should be continuous-average + intercept
 assert(1==near(ufresult_marginal.beta(:,:,1), sum(ufresult_average.beta(:,:,[1,3]),3)))
+
+
+% Testing Bug #82
+%%
+EEG = simulate_test_case(15,'noise',0,'basis','box');
+for e = 1:length(EEG.event)
+    EEG.event(e).type= ['test_' num2str(randi(6,1))];
+end
+cfgDesign = [];
+cfgDesign.coding = 'dummy';
+cfgDesign.formula   = {'y~1+continuousA','y~1+continuousA'};
+cfgDesign.eventtypes = {{'test_1','test_5','test_3'},{'test_3','test_5'}};
+
+EEG = uf_designmat(EEG,cfgDesign);
+EEG = uf_timeexpandDesignmat(EEG,'timelimits',[-0.5 1.5]);
+
+EEG = uf_glmfit(EEG);
+
+ufresult = uf_condense(EEG);
+
+ufresult_predict = uf_predictContinuous(ufresult,'predictAt',{{'continuousA',[0 1]},{'contB',[0 1]},{'splineA',[-1 0 1]}});
+
+% this used to throw an error due to the event matching (because we want to
+% add the marginal only to the event it belongs to).
+ufresult_marginal = uf_addmarginal(ufresult_predict);
 
 
 
