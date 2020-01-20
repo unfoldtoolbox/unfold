a9a41fc4150263904a18c3638609e91b1c8f4ab0
Benedikt Ehinger <behinger@uos.de>
2020-01-20 12:37:13 +0100

    updated erpimage, fixed bug & added new option

--- src/uf_toolbox/uf_erpimage.m
+++ src/uf_toolbox/uf_erpimage.m
@@ -1,11 +1,9 @@
+
 function [varargout] = uf_erpimage(EEG,varargin)
-% This function produces ERPimages with/without modelled data.
-% There is no support for group-level data (yet) you would need to trick
-% the function somehow :S
+
 assert(isfield(EEG,'data'))
 assert(ndims(EEG.data) == 2) % data have to be continuous
 if 1 == 0
-    % Example call, will be removed for release
     %%
     cfgtmp = [];
     cfgtmp.type = 'deconv';
@@ -20,26 +18,19 @@ if 1 == 0
     cfgtmp.searchtime = [-10 10];
     
 end
-% raw = default erpimage on raw data
-% fullmodel = all effects are taken into account
-% residuals = raw - fullmodel
-% deconv/nodeconv = by default same as fullmodel, but with "remove" and "keep" can
-% be further specified (define only remove OR keep). Can be either based on
-% overlap corrected ERPs or non-overlap corrected ones
-% 
 cfg = finputcheck(varargin,...
-    {'type','string',{'raw','deconv','nodeconv','residuals','fullmodel'},'deconv'; % 
-    'remove','cell',[],{};     % which event(s) predictor(s) pair(s) to remove {'eventA',{'(Intercept)','stimA'}} (or a cell array of such cell arrays)
-    'keep','cell',[],{};       % which event(s) predictor(s) pair(s) to keep   {'eventA',{'stimB'}} (or a cell array of such cell arrays)
-    'alignto','cell',[],{};    % what should be the event to align the erpimage?
-    'sort_align','',[],[];     % default same as alignto
-    'sort_by','',[],'latency'; % could be in plotting function? What Eventfield to sort by
-    'split_by','',[],[];       % make multiple subplots with separate ERPimages
-    'winrej','real',[],[];     % remove parts of the continuous data
-    'plot','boolean',[],0;     % if called without requesting output,
-    'searchtime','real',[],[]; % when to when to look for the sort_align event
-    'timelimits','real',[],[]; %from when to when
-    'channel','integer',[],[]; 
+    {'type','string',{'raw','deconv','nodeconv','residuals','fullmodel','deconvDirect'},'deconv'; % deconv erpimage or not
+    'remove','cell',[],{};   % which event(s) predictor(s) pair(s) to remove {'eventA',{'(Intercept)','stimA'}} (or a cell array of such cell arrays)
+    'keep','cell',[],{};     % which event(s) predictor(s) pair(s) to keep   {'eventA',{'stimB'}} (or a cell array of such cell arrays)
+    'alignto','cell',[],{};  % what should be the event to align the erpimage?
+    'sort_align','',[],[];   % default same as alignto
+    'sort_by','',[],'latency';   % could be in plotting function? What Eventfield to sort by
+    'split_by','',[],[]; % make multiple subplots with separate ERPimages
+    'winrej','real',[],[];
+    'plot','boolean',[],0;  % if called without requesting output,
+    'searchtime','real',[],[];
+    'timelimits','real',[],[];
+    'channel','integer',[],[];
     'figure','boolean',[],0;
     'caxis','real',[],[];
     },'mode','ignore');
@@ -54,7 +45,7 @@ if cfg.figure
     figure
 end
 %%
-if any(strcmp(cfg.type,{'raw','nodeconv','residuals'}))
+if any(strcmp(cfg.type,{'raw','nodeconv','deconvDirect','residuals'}))
     fprintf('Epoching EEG in case of nodeconv')
     EEG_epoch = uf_epoch(EEG,struct('winrej',cfg.winrej,'timelimits',EEG.unfold.times([1,end])+[0 diff(EEG.unfold.times([1:2]))]));
     EEG_epoch = uf_glmfit_nodc(EEG_epoch,'channel',cfg.channel);
@@ -119,62 +110,77 @@ switch cfg.type
         beta = ufresult.beta(cfg.channel,:,keep);
         assert(~all(isnan(beta(:))),'found only nans, did you specify & calculate the correct channel?')
         data = ufresult.unfold.Xdc(:,ismember(ufresult.unfold.Xdc_terms2cols,keep))*beta(:);
+    case 'deconvDirect'
+        beta = squeeze(ufresult.beta(cfg.channel,:,keep));
+        data = ufresult.unfold.X(:,keep)*beta';
+        data = data';
+        
     case 'nodeconv'
-        beta = ufresult.beta_nodc(cfg.channel,:,keep);
-        data = ufresult.unfold.Xdc(:,ismember(ufresult.unfold.Xdc_terms2cols,keep))*beta(:);
+        beta = squeeze(ufresult.beta_nodc(cfg.channel,:,keep));
+        data = ufresult.unfold.X(:,keep)*beta';
         data = data';
     case {'residuals','fullmodel'}
         beta = ufresult.beta(cfg.channel,:,:);
         data = ufresult.unfold.Xdc*beta(:);
+        
         
 end
 
 %%
 % overwrite old data
 EEG_new = EEG;
-if strcmp(cfg.type,'raw')
-    EEG_new_epoch = EEG_epoch;
-    EEG_new_epoch.data = EEG_new_epoch.data(cfg.channel,:,:);
-else
-    EEG_new.icaact = [];EEG_new.icasphere = [];EEG_new.icaweights= [];
-    EEG_new.data = data';
-    EEG_new_epoch = uf_epoch(EEG_new,'timelimits',ufresult.unfold.times([1,end])+[0 diff(ufresult.times([1:2]))],'winrej',cfg.winrej);
+switch cfg.type
+    case 'raw'
+        
+        EEG_modelled = EEG_epoch;
+        EEG_modelled.data = EEG_modelled.data(cfg.channel,:,:);
+    case {'nodeconv','deconvDirect'}
+        EEG_modelled = EEG_epoch;
+        EEG_modelled.data = permute(data,[3 1 2]);
+    case {'deconv','residuals','fullmodel'}
+        % first generate continuous data, for this remove the ICA info else
+        % we get errors
+        EEG_new.icaact = [];EEG_new.icasphere = [];EEG_new.icaweights= [];
+        EEG_new.data = data';
+        % now cut the data once more. This reintroduces overlapping potentials
+        % - exactly what we want here
+        EEG_modelled = uf_epoch(EEG_new,'timelimits',ufresult.unfold.times([1,end])+[0 diff(ufresult.times([1:2]))],'winrej',cfg.winrej);
 end
 
 if strcmp(cfg.type,'residuals')
-    EEG_new_epoch.data = EEG_new_epoch.data -  EEG_epoch.data;
+    EEG_modelled.data = EEG_modelled.data -  EEG_epoch.data(cfg.channel,:,:);
 end
-[keep_epoch]= ~isnan(eeg_getepochevent(EEG_new_epoch,cfg.alignto,[0,0],'type')); % output in ms
-EEG_new_epoch.data = EEG_new_epoch.data(:,:,keep_epoch);
+[keep_epoch]= ~isnan(eeg_getepochevent(EEG_modelled,cfg.alignto,[0,0],'type')); % output in ms
+EEG_modelled.data = EEG_modelled.data(:,:,keep_epoch);
 
-sort_vector = eeg_getepochevent(EEG_new_epoch,cfg.sort_align,cfg.searchtime,cfg.sort_by); % output in ms
+sort_vector = eeg_getepochevent(EEG_modelled,cfg.sort_align,cfg.searchtime,cfg.sort_by); % output in ms
 sort_vector = sort_vector(keep_epoch);
 
 %% draw ERPimage
 if isempty(cfg.caxis)
-    cfg.caxis = prctile(EEG_new_epoch.data(:),[5 95]);
+    cfg.caxis = prctile(EEG_modelled.data(:),[5 95]);
     cfg.caxis = [-max(abs(cfg.caxis)) max(abs(cfg.caxis))];
 end
 if nargout == 0 || cfg.plot == 1
     if isempty(cfg.split_by)
         
-    
-    erpimage(EEG_new_epoch.data,sort_vector,EEG_new_epoch.times,'',10,0,'caxis',cfg.caxis);
+        
+        erpimage(EEG_modelled.data,sort_vector,EEG_modelled.times,'',10,0,'caxis',cfg.caxis);
     else
-       evt_tmp = {EEG.event.(cfg.split_by)};
-       evt = evt_tmp(cellfun(@(x)~isempty(x),evt_tmp));
-       splitlevel = unique(evt);
-       n_splits = length(splitlevel);
-       for n = 1:n_splits
-           subplot(n_splits,1,n)
-           evt_ix = strcmp(evt,splitlevel{n});
-           erpimage(EEG_new_epoch.data(:,:,evt_ix),sort_vector(evt_ix),EEG_new_epoch.times,'',10,0,'caxis',cfg.caxis);
+        evt_tmp = {EEG.event.(cfg.split_by)};
+        evt = evt_tmp(cellfun(@(x)~isempty(x),evt_tmp));
+        splitlevel = unique(evt);
+        n_splits = length(splitlevel);
+        for n = 1:n_splits
+            subplot(n_splits,1,n)
+            evt_ix = strcmp(evt,splitlevel{n});
+            erpimage(EEG_modelled.data(:,:,evt_ix),sort_vector(evt_ix),EEG_modelled.times,'',10,0,'caxis',cfg.caxis);
             title(sprintf('split by:%s, level:%s, effect of:%s',cfg.split_by,splitlevel{n},cfg.keep{1}{2}{:}))
-       end
+        end
     end
 else
     
-    outdata = EEG_new_epoch.data;
+    outdata = EEG_modelled.data;
     outsort = sort_vector;
     varargout{1} = outdata;
     varargout{2} = outsort;
