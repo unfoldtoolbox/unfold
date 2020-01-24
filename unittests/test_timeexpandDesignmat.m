6842c7ffcdd1bbdcd868e9189c70468871e89424
Benedikt Ehinger <behinger@uos.de>
2020-01-24 10:58:32 +0100

    added trf tutorial

--- unittests/test_timeexpandDesignmat.m
+++ unittests/test_timeexpandDesignmat.m
@@ -14,7 +14,7 @@ cfgDesign.eventtypes = {'stimulusA'};
 
 EEGsim = uf_designmat(EEGsim,cfgDesign);
 %%
-EEG2 = uf_timeexpandDesignmat(EEGsim,'timelimits',[-1 2])
+EEG2 = uf_timeexpandDesignmat(EEGsim,'timelimits',[-1 2]);
 
 
 events = sum(strcmp({EEG2.event(:).type},'stimulusA'));
@@ -23,7 +23,7 @@ assert(events==timeshiftedvals)
 % We had a bug where only negative entries did not timeexpand it
 EEG2 = EEGsim;
 EEG2.unfold.X = -EEG2.unfold.X;
-EEG2 = uf_timeexpandDesignmat(EEG2,'timelimits',[-1 2])
+EEG2 = uf_timeexpandDesignmat(EEG2,'timelimits',[-1 2]);
 
 events = sum(strcmp({EEG2.event(:).type},'stimulusA'));
 timeshiftedvals = sum(EEG2.unfold.Xdc(:,1));
