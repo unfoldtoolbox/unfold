a43c03ea15b2c43e08aab8caa899f1a8b4664879
Benedikt Ehinger <behinger@uos.de>
2020-05-06 00:27:28 +0200

    new experimental feature: addmarginal of splines in uf_se

--- unittests/test_se.m
+++ unittests/test_se.m
@@ -40,4 +40,43 @@ contrast(3,uf.unfold.times>3,2) = 1;
 
 se = uf_se(uf,'channels',[1],'contrast',contrast);
 
+%% Now the hard part. Addmarginal
+EEGsim = simulate_test_case(5,'noise',1,'basis','posneg','srate',50,'datalength',10*60);
+cfgDesign = [];
 
+
+cfgDesign.eventtypes = {'stimulusA'};
+cfgDesign.codingschema = 'effects'
+cfgDesign.formula   = {'y~1+spl(continuousA,5)'};
+% cfgDesign.formula   = {'y~1+continuousA'};
+
+
+uf = uf_designmat(EEGsim,cfgDesign);
+uf = uf_timeexpandDesignmat(uf,'timelimits',[-.3 5.5]);
+uf = uf_glmfit(uf);
+
+
+% test the contrast
+contrast = zeros(1,size(uf.unfold.beta_dc,2),size(uf.unfold.beta_dc,3));
+contrast(1, 29,1) = 1;
+[se] = uf_se(uf,'contrast',contrast,'spline_addmarginal',1);
+
+contrast = zeros(size(uf.unfold.beta_dc,2),size(uf.unfold.beta_dc,2),size(uf.unfold.beta_dc,3));
+for c = 1:size(contrast,1)
+    contrast(c,c,1) = 1;
+end
+
+%%
+for spl_addmarginal = [0 1]
+[se,contrast2] = uf_se(uf,'contrast',contrast,'spline_addmarginal',spl_addmarginal);
+m = contrast2(:,:)*uf.unfold.beta_dc(1,:)';
+if spl_addmarginal == 0
+figure,
+else
+hold on
+end
+errorbar(m,se)
+end
+plot(uf.unfold.beta_dc(1,:,1))
+tmp_uf = uf_addmarginal(uf_predictContinuous(uf_condense(uf)));
+plot(tmp_uf.beta(1,:,1))
